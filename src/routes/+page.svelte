c498cf1874bd7a3159644d2ccf5b1a2030ac21cb
Joseph Liu <josephliu24@outlook.com>
2024-02-11 16:09:03 -0500

    abridged

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -42,31 +42,8 @@
             <p class="h1">
                 About Me
             </p>
-            <p class="text">
-                Hello! I'm Joseph Liu, a third year undergraduate student at Georgia Tech with concentrations in
-                Systems/Architecture and Artificial Intelligence. I'm passionate and interested in computer systems,
-                distributed & high performance computing, and machine learning.
-            <p class="text">
-                Through my coursework and projects I've gained lots of experience with different techniques and
-                technologies. Through my systems coursework I've learned about OS design, virtualization, and many aspects
-                of distributed system design such as synchronization, distributed memory and storage, and algorithms like
-                MapReduce which can be used to parallelize queries to different services. I've learned a lot about AI as
-                well, including machine learning, computer vision, robotics, and technologies like Jupyter, NumPy, and
-                PyTorch. Through projects, I've also gained experience with web and app technologies like Node.js, React.js,
-                React Native, Svelte, and more.
-            <p class="text">
-                In the summer of 2023, I interned as a software engineer at GEICO, where worked with the Service Enablement
-                team. During my time there, I learned more about how companies take advantage of cloud computing and some
-                basics with technologies like Kubernetes, Microsoft Azure, and .NET Web Services.
-                I audited the company's service portfolio and began work on building a new service portfolio which would
-                make it easier to keep service metadata accurate and up-to-date and have better integrations with ITSM and
-                other services.
-            <p class="text">
-                Beyond computer science and software development, I have experience with media technologies and creative
-                applications. Previously, I served as the VP of Media at Lynbrook Robotics, where I learned creative tools
-                such as Adobe Photoshop, Premiere Pro, and other Creative Cloud tools. In my free time I also do
-                photography, illustration, and graphic design.
-            </p>
+            <p class="text">Hello, I'm Joseph, a third-year undergraduate at Georgia Tech, specializing in Systems/Architecture and Artificial Intelligence. My passion lies in exploring the intricacies of computer systems, distributed &amp; high-performance computing, and machine learning.</p>
+            <p class="text">My academic journey has equipped me with a strong foundation in <strong>OS design</strong>, <strong>virtualization</strong>, and <strong>distributed system design</strong>, including synchronization, distributed memory and storage, and parallelizing queries using MapReduce. Additionally, I've delved into the realm of AI, gaining insights into <strong>machine learning</strong>, <strong>computer vision</strong>, and <strong>robotics</strong>, and becoming proficient in tools like Jupyter, NumPy, and PyTorch.</p>
         </div>  
     </SideBySide>
 </Body>
